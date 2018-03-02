1f396e8d669d86cce7ca3f350d7fc90ad93e6735
Dre <31423756+jinxTelesis@users.noreply.github.com>
2018-03-02 12:54:40 -0500

    Create Romanlab1v1.cpp

---
+++ Romanlab1v1.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <iostream>
+#include "RomanType.h"
+
+void RomanType::supportRomanToIntFunction(int factor, int & sum, int & previous) {
+	sum += factor;
+	if (previous < factor)
+		sum -= 2 * previous;
+	previous = factor;
+}
+
+void RomanType::romanToPostiveinteger() {
+	int previous = 1000, sum = 0, i, length = romanNum.length();
+
+	for (i = 0; i < length; i++) {
+		switch (romanNum[i]) {
+			case 'M':
+				supportRomanToIntFunction(1000, sum, previous);
+				break;
+			case 'D':
+				supportRomanToIntFunction(500, sum, previous);
+				break;
+			case 'C':
+				supportRomanToIntFunction(100, sum, previous);
+				break;
+			case 'L':
+				supportRomanToIntFunction(50, sum, previous);
+				break;
+			case 'X':
+				supportRomanToIntFunction(10, sum, previous);
+				break;
+			case 'V':
+				supportRomanToIntFunction(5, sum, previous);
+				break;
+			case 'I':
+				supportRomanToIntFunction(1, sum, previous);
+				break;
+
+		}
+	}
+	num = sum;
+}
+
+RomanType::RomanType()
+{
+	romanNum = "";
+}
+
+RomanType::RomanType(std::string romanNum)
+{
+	this->romanNum = romanNum;
+}
+
+void RomanType::setRoman(std::string romanNum)
+{
+	this->romanNum = romanNum;
+}
+
+void RomanType::printRoman()
+{
+	std::cout << "the roman numeral " <<romanNum << std::endl;
+}
+
+void RomanType::printPositiveinteger()
+{
+	std::cout << "equals " << num << " as an integer " << std::endl;
+}
